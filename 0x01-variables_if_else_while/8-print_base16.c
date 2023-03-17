2c297a091a77c0c49cea79dbc70bb4c9d731b3c7
khaledibrahim7 <khaledramadanza@gmail.com>
2023-03-17 08:41:28 -0700

    print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,10 @@
 
 /**
  * main - prints all the numbers of base 16 in lowercase,
-
  * followed by a new line
-
  * Return: Always 0 (Success)
 
  */
-
 int main(void)
 
 {
@@ -21,21 +18,13 @@ char ch;
 
 for (n = 48; n < 58; n++)
 
-        {
-
+{
 putchar(n);
-
-        }
-
+}
 for (ch = 'a'; ch <= 'f'; ch++)
-
-        {
-
+{
 putchar(ch);
-
-        }
-
+}
 putchar('\n');
-
 return (0);
 }
