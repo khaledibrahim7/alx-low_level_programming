0aa813cacab53b6538412e3277ccebff68677aeb
khaledibrahim7 <khaledramadanza@gmail.com>
2023-03-17 08:38:54 -0700

    print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,11 +3,8 @@
 
 /**
  * main - prints the lowercase alphabet in reverse,
-
  * followed by a new line
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
@@ -19,13 +16,10 @@ char ch;
 
 for (ch = 'z'; ch >= 'a'; ch--)
 
-        {
+{
 putchar(ch);
-
-        }
-
+}
 putchar('\n');
-
 return (0);
 
 }
