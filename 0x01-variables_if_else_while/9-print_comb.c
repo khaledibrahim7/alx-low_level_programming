e742e68c9ef2326571f3f26f48cb26b474e1be8b
khaledibrahim7 <khaledramadanza@gmail.com>
2023-03-17 08:46:47 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,11 +3,8 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
 
 {
@@ -20,21 +17,13 @@ for (n = 48; n < 58; n++)
         {
 
 putchar(n);
-
 if (n != 57)
-
-                {
-
+{
 putchar(',');
-
 putchar(' ');
-
                 }
-
         }
 
 putchar('\n');
-
 return (0);
-
 }
