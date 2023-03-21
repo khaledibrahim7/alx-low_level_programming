1ffc3ca5d64f3af526de4bba0160f4d94ef11462
khaledibrahim7 <khaledramadanza@gmail.com>
2023-03-21 15:56:55 -0700

    fix syntax error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
@@ -28,5 +29,6 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-_putchar('\n');	}
+_putchar('\n');
+}
 }
