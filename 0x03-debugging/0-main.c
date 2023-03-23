03bf27fd6920dddaa713014f262ccdc86c8cefc4
khaledibrahim7 <khaledramadanza@gmail.com>
2023-03-23 16:20:15 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,16 +8,7 @@
 int main(void)
 {
  int i;
-
  i = 0;
  positive_or_negative(i);
-
-return (0);
-}
-
-void positive_or_negative(int i){
-if (i >= 0)
-        printf("%d is a positive number \n", i);
-    else
-        printf("%d is a negative number \n", i);
+ return (0);
 }
